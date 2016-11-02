56385a2f42ed60891ac2fdac62e3bc6916f5331e
DanielGoez <daniel.gmz@hotmail.com>
2016-11-02 16:19:21 -0500

    Menu terminado con inputs
    Adicione inputs a la funcion menu. Para no usar inputs en el resto del codigo

--- Codigo.py
+++ Codigo.py
@@ -30,10 +30,12 @@ class Biblioteca(object):
         while opcion!=0:
             print ("1. Agregar estudio")
             print ("2. Agregar factor pronostico")
-            print ("3. Consultar Estudios")
-            print ("4. Consultar Estadisticos de un factor pronostico (Asociados a un estudio)")
-            print ('5. Consultar Estadisticos de todos los factores pronostico (Asociados a un estudio)')
-            print ('6. Graficar una variable fisiologica asociada a un estudio')
+            print ('3. Mostrar todos los Estudios')
+            print ("4. Consultar Estudios")
+            print ("5. Consultar Estadisticos de un factor pronostico")
+            print ('6. Consultar Estadisticos de todos los factores pronostico')
+            print ('7. Consultar por palabra clave')
+            print ('8. Graficar una variable fisiologica asociada a un estudio')          
             print ("0. Salir")
             try:    
                 opcion = int(input('Ingrese el número de la opción que desea...  '))
@@ -44,35 +46,62 @@ class Biblioteca(object):
                 Estudio.__init__(self)
                 
             elif opcion == 2:
+                #Ejecuta el buscador de estudio y el estudio que elija el usuario es asignado a paciente
+                paciente = Estudio.busc_estudios(self)
                 #Ejecuta el inicializador de la clase Var_Fis
-                Var_Fis.__init__(self)
-                
+                if paciente == None:
+                    print ('Ingrese un estudio existente. (Debe crear un estudio antes de asignarle un diagnostico)')
+                else: 
+                    variable_fis = Var_Fis.__init__(self)
+                    Estudio.add_var_fis(self, variable_fis)                 
+            
             elif opcion == 3:
-                #Ejecuta la funcion buscar estudios de la clase Estudio
+                #Ejecuta la funcion buscar estudios de la clase Estudio e imprime todos los objetos tipo estudio
                 Estudio.busc_estudios(self)
                 
             elif opcion == 4:
-                #Ejecuta la funcion buscar un estadistico de un estudio de la clase Var_Fis
-                Var_Fis.busc_var_fis_estadisticos_unitario(self)
+                #Recibe el valor que el usuario desea buscar
+                key = input('Ingrese el nombre del estudio  ')
+                #Ejecuta la funcion buscar estudios de la clase Estudio con key como argumento
+                busqueda = Estudio.busc_estudios(self,key)
+                if busqueda == None:
+                    print('Debe crear por lo menos un estudio antes de poder buscar')
                 
             elif opcion == 5:
-                #Ejecuta la funcion buscar todos los estadistico de un estudio de la clase Var_Fis
-                Var_Fis.busc_var_fis_estadisticos_todos(self)
+                #Recibe el valor que el usuario desea buscar
+                key = input('Ingrese el nombre del factor pronostico o variable fisiologica  ')
+                #Ejecuta la funcion buscar un estadistico de un estudio de la clase Var_Fis con key como argumento
+                busqueda = Var_Fis.busc_var_fis_estadisticos_unitario(self,key)
+                if busqueda == None:
+                    print('Debe crear por lo menos un estudio y asignarle una variable fisiologica antes de poder buscar')
                 
             elif opcion == 6:
-                #Ejecuta la funcion buscar estudios de la clase estudios. Asigna el valor a estudio
-                estudio = Estudio.busc_estudios(self)
-                #Ejecuta la funcion buscar variable fisiologica asociada a un estudio. Asigna el valor a Variable_Fis
-                Variable_Fis = Var_Fis.busc_var_fis(estudio)
-                #Utiliza la funcion graficar de la clase Var_Fis. Toma como argumento la variable Variable_Fis 
-                Var_Fis.graficar(Variable_Fis)
-                
-                
-                
+                #Recibe el valor que el usuario desea buscar
+                key = input('Ingrese el nombre del factor pronostico o variable fisiologica  ')
+                #Ejecuta la funcion buscar todos los estadistico de un estudio de la clase Var_Fis
+                busqueda = Var_Fis.busc_var_fis_estadisticos_todos(self,key)
+                if busqueda == None:
+                    print('Debe crear por lo menos un estudio y asignarle una variable fisiologica antes de poder buscar')
             
-        
-        Pass
-        
+            elif opcion == 7:
+                #Recibe el valor que el usuario desea buscar
+                key = input('Ingrese la palabra clave  ')               
+                #ejecuta el buscador por palabra d ela clase biblioteca
+                busqueda = Biblioteca.buscador(self)
+                if busqueda == None:
+                    print('Debe crear por lo menos un estudio antes de poder buscar')
+                
+            elif opcion == 8:
+                #Ejecuta la funcion buscar estudios de la clase estudios. Asigna el valor a estudio
+                input('Ingrese el nombre del estudio donde se encuentra la variable que desea graficar. Luego elija el estudio y la variable fisiologica  ')
+                estudio = Estudio.busc_estudios(self)
+                if estudio == None:
+                    print('Ingrese un estudio existente. (Debe crear un estudio y asignarle una variable fisiologica para graficar)' )
+                else:
+                    #Ejecuta la funcion buscar variable fisiologica asociada a un estudio. Asigna el valor a Variable_Fis
+                    Variable_Fis = Var_Fis.busc_var_fis(self,estudio)
+                    #Utiliza la funcion graficar de la clase Var_Fis. Toma como argumento la variable Variable_Fis 
+                    Var_Fis.graficar(self,Variable_Fis)     
             
             
 class Estudio(object):
