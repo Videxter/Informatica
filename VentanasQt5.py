8d462781ac5a96c144b78f5713976ee7eb40150a
DanielGoez <daniel.gmz@hotmail.com>
2016-11-16 20:58:31 -0500

    Vent ppal

--- VentanasQt5.py
+++ VentanasQt5.py
@@ -14,7 +14,8 @@ class SignalSlot_window(QDialog):
         
         btn0= QPushButton('Agregar Estudio')
         btn1= QPushButton('Agregar Factor Pronostico')
-        btn2= QPushButton('Mostrar todos los estudios')
+        btn2= QPushButton('Buscar')
+        btn3= QPushButton('Mostrar todos los estudios')
         
         title1 = QLabel('Palabra clave')
         title2 = QLabel('Fecha de Nacimiento')
@@ -25,6 +26,7 @@ class SignalSlot_window(QDialog):
         title7 = QLabel('Ingreso de datos')
         title8 = QLabel('Busqueda')
         
+        
         plbra_clave = QLineEdit()
         fecha_nac = QLineEdit()
         name = QLineEdit()
@@ -32,9 +34,11 @@ class SignalSlot_window(QDialog):
         patologia = QLineEdit()
         fechaFP = QLineEdit()
         
+
         Grid.addWidget(btn0,1,0)
         Grid.addWidget(btn1,2,0)
         Grid.addWidget(btn2,10,0)
+        Grid.addWidget(btn3,10,1)
         Grid.addWidget(plbra_clave,4,1)
         Grid.addWidget(fecha_nac,5,1)
         Grid.addWidget(name,6,1)
@@ -51,16 +55,25 @@ class SignalSlot_window(QDialog):
         Grid.addWidget(title7,0,0)
         Grid.addWidget(title8,3,0)
 
+        
         self.setLayout(Grid)
         self.setWindowTitle('Pronostico de Enfermedades')        
         
+        '''
         btn0.clicked.connect()        
         btn1.clicked.connect()
         btn2.clicked.connect()
+        btn3.clicked.connect()
+        '''
+    '''    
+    def ClicBoton(self):
+        sender= self.sender()
+        print('Se presionó el '+ sender.text())
+    '''
+
         
 if __name__== '__main__':
     app = QApplication(sys.argv)
     vent = SignalSlot_window()
     vent.show()
     sys.exit(app.exec_())
-        
